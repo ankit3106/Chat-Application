a38ef880876695fab7e2c1b48f1e94b51d47d39f
Ankit <luffytarow13@gmail.com>
2024-12-21 16:12:38 +0530

    ui part complete

--- ChatApp/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ ChatApp/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets' // This is the assets folder we created in the src folder
 
 const ProfileUpdate = () => {
+
+  const [image, setImage] = React.useState(false);
+
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form >
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <img src={image?URL.createObjectURL(image):assets.avatar_icon} alt="" />
+            upload profile picture
+          </label>
+          <input type="text" placeholder='Your Name' required />
+          <textarea placeholder='Write profile bio' required></textarea>
+          <button type='submit'>Save</button>
+        </form>
+        <img className='profile-pic' src={image?URL.createObjectURL(image):assets.logo_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
